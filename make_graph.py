24e1d5552984f50856cc26c952b3c7068bd8fe4b
strikingLoo <lucianostrika44@gmail.com>
2023-03-15 02:33:25 -0700

    missing: adding url to nodes

--- make_graph.py
+++ make_graph.py
@@ -1,12 +1,26 @@
 import json
 import networkx as nx
 from bokeh.palettes import Category20
-from bokeh.plotting import figure, from_networkx, show
+from bokeh.plotting import figure, from_networkx, show, output_file
+
+output_file('wiki_graph.html')
 
 # Load the JSON file
 with open('output.json', 'r') as f:
     node_data = json.load(f)
 
+with open('../../dev/strikingloo.github.io/_site/graph_data.json') as f:
+    names_data = json.load(f)
+
+url_to_metadata = {item.get('url','')[6:] : {'title': item['title'], 'url': item.get('url','')} for item in names_data['data']}
+
+print(url_to_metadata)
+def get_title(name):
+    return url_to_metadata[name]['title']
+
+def get_url(name):
+    return url_to_metadata[name]['url']
+    
 seen = {}
 node_to_id = {}
 node_id = 0
@@ -15,7 +29,7 @@ G = nx.Graph()
 for node, _ in node_data.items():
     node_to_id[node] = node_id
     node_id+=1
-    G.add_node(node_to_id[node], name = node)
+    G.add_node(node_to_id[node], name = get_title(node), url = get_url(node))
 
 for node, edges in node_data.items():
     for dest in edges:
@@ -31,7 +45,6 @@ graph = from_networkx(G, nx.spring_layout, scale=30, center=(0,0))
 # Add some new columns to the node renderer data source
 graph.node_renderer.data_source.data['index'] = list(range(len(G)))
 graph.node_renderer.data_source.data['colors'] = (Category20[20]*5)[:len(G)]
-
 graph.node_renderer.glyph.update(size = 20, fill_color="colors")
 
 p.renderers.append(graph)
