478614b9b9f82fca3c3fced5f6498cc1368a3c16
strikingLoo <lucianostrika44@gmail.com>
2023-03-17 02:03:45 -0700

    make graph have links!!

--- make_graph.py
+++ make_graph.py
@@ -3,6 +3,11 @@ import networkx as nx
 from bokeh.palettes import Category20
 from bokeh.plotting import figure, from_networkx, show, output_file
 
+
+from bokeh.models import ColumnDataSource, TapTool,CustomJS, OpenURL, Circle
+from bokeh.embed import file_html
+from bokeh.resources import INLINE
+
 output_file('wiki_graph.html')
 
 # Load the JSON file
@@ -14,7 +19,6 @@ with open('../../dev/strikingloo.github.io/_site/graph_data.json') as f:
 
 url_to_metadata = {item.get('url','')[6:] : {'title': item['title'], 'url': item.get('url','')} for item in names_data['data']}
 
-print(url_to_metadata)
 def get_title(name):
     return url_to_metadata[name]['title']
 
@@ -37,16 +41,25 @@ for node, edges in node_data.items():
 
 print(G)
 
-p = figure(tools="hover,pan,wheel_zoom,box_zoom,reset", tooltips="@name", width=720, height=720, x_axis_location=None, y_axis_location=None)
+p = figure(tools="hover,pan,wheel_zoom,box_zoom,reset,tap", tooltips="@name",  width=720, height=720, x_axis_location=None, y_axis_location=None)
 p.grid.grid_line_color = None
 
-graph = from_networkx(G, nx.spring_layout, scale=30, center=(0,0))
+pos = nx.spring_layout(G, k=0.15, iterations=20)
+
+graph = from_networkx(G, pos, scale=30, center=(0,0))
 
 # Add some new columns to the node renderer data source
 graph.node_renderer.data_source.data['index'] = list(range(len(G)))
 graph.node_renderer.data_source.data['colors'] = (Category20[20]*5)[:len(G)]
 graph.node_renderer.glyph.update(size = 20, fill_color="colors")
+graph.node_renderer.selection_glyph = Circle(size = 20, fill_color="colors")
+graph.node_renderer.nonselection_glyph = Circle(size = 20, fill_color="colors")
+graph.name = 'graphy'
 
+url = "https://strikingloo.github.io@url"
+taptool = p.select(type=TapTool)
+taptool.callback = OpenURL(url=url)
 p.renderers.append(graph)
+
 
 show(p)
